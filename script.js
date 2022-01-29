1c4b569f94676658168d3a1e757007e81552c8c2
Jeremy Wolfe <jeremy.t.wolfe@outlook.com>
2022-01-29 18:59:37 -0500

    Finished Pente!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//This script plays a game of tic-tac-toe between two human players
+//This script plays a game of Pente between two human players
 //
 //The main goal of the scripting exercise is to use as little global
 //code as possible by utilizing modules/factories.
@@ -8,24 +8,21 @@
   //GameFlow module
   //playerOne Object
   //playerTwo Object
-//
-//
-//The code is also designed with expandability in mind.
-//It will later become the foundation for a game-project called 'Pente'.
 
 
 //Create a GameBoard module
 //This:
-//hears from GameFlow module, and Player units
-//speaks to the GameFlow module
+//hears from GameFlow module and Player units
+//speaks to the GameFlow module and Player units
 //acts as main UI
 const GameBoard = ( () => {
   const GRID_SIZE = 17;
   const WINNING_LENGTH = 5;
-  let spacesFilled = 0;
 
-  //displayBoard: displays the play area grid to a specified length
+  let spacesFilled = 0;
+  //displayBoard: displays the play area grid and game info
   const displayBoard = function() {
+    //first, the play area grid
     let board = document.getElementById('gameBoard');
     for (let x = 0; x < GRID_SIZE; x++) {
       for (let y = 0; y < GRID_SIZE; y++) {
@@ -35,20 +32,30 @@ const GameBoard = ( () => {
         board.appendChild(div);
       }
     }
-    playerOne.scoreboard.textContent = `${playerOne.name}: ${playerOne.score}`;
-    playerTwo.scoreboard.textContent = `${playerTwo.name}: ${playerTwo.score}`;
+    //then game info for each player: captures and scores.
+    //captures:
+    const captureDivList = document.querySelectorAll('.captures');
+    const pOneCaptureDiv = captureDivList[0];
+    const pTwoCaptureDiv = captureDivList[1];
+    pOneCaptureDiv.setAttribute('id', playerOne.name);
+    pTwoCaptureDiv.setAttribute('id', playerTwo.name);
+
+    //scores:
+    playerOne.scoreboard.textContent = `${playerOne.name}: ${playerOne.gameScore}`;
+    playerTwo.scoreboard.textContent = `${playerTwo.name}: ${playerTwo.gameScore}`;
   }
 
-  //resetBoard: resets the page for a new game
-  const resetBoard = function() {
+  //resetGame: resets the page for a new game
+  const resetGame = function() {
     location.reload();
   }
 
-  //makeMove: place symbol onto  array, update HTML, end turn
+  //makeMove: place piece onto  array, update HTML, end turn
   //the function's -this- is bound through a PlayerFactory function
   const makeMove = function(square) {
     let div = document.createElement('div');
     div.classList.toggle('occupied');
+    div.setAttribute('id', GameFlow.activePlayer.color)
     square.appendChild(div);
     //square.textContent = this;
     let id = square.getAttribute('id');
@@ -57,7 +64,7 @@ const GameBoard = ( () => {
     let y = Number(idArray[1]);
     if (isWon(x, y)) {
       spacesFilled = 0;
-      GameFlow.activePlayer.score++;
+      GameFlow.activePlayer.gameScore++;
       sayVictory(GameFlow.activePlayer);
     } else {
       spacesFilled++;
@@ -73,18 +80,24 @@ const GameBoard = ( () => {
     console.log(`Unable to place ${square} there!`);
   }
 
-  //findOccupant: return a space's occupying symbol
-  const findOccupant = function(x, y) {
+  const isOccupied = function(x, y) {
+    let gridSquare = document.getElementById(`${x} ${y}`);
+    if (gridSquare.firstChild) return true;
+    return false;
+  }
+
+  //ownsOccupant: return true if player occupies square
+  const ownsOccupant = function(x, y) {
     let gridSquare = document.getElementById(`${x} ${y}`);
     try {
-      if (!gridSquare.textContent.length) {
+      if (!gridSquare.firstChild) {
         return false;
       }
     } catch {
       return false;
     }
-    let text = gridSquare.textContent;
-    if (text != GameFlow.activePlayer.symbol) {
+    let squareId = gridSquare.firstChild.getAttribute('id');
+    if (squareId != GameFlow.activePlayer.color) {
       return false;
     }
     return true;
@@ -101,32 +114,39 @@ const GameBoard = ( () => {
       }
     }
     //horizontal check
-    tempPoints = rowCheck(x, y, 1, 0) 
+    tempPoints = rowScoreCheck(x, y, 1, 0);
+    let coordArray = rowCaptureCheck(x, y, 1, 0);
+    if (coordArray) capture(x, y, coordArray[0], coordArray[1]);
     findHighest();
     //verical check
-    tempPoints = rowCheck(x, y, 0, 1) 
+    tempPoints = rowScoreCheck(x, y, 0, 1);
+    coordArray = rowCaptureCheck(x, y, 0, 1);
+    if (coordArray) capture(x, y, coordArray[0], coordArray[1]);
     findHighest();
     //slope +1 diagonal check
-    tempPoints = rowCheck(x, y, 1, 1) 
+    tempPoints = rowScoreCheck(x, y, 1, 1);
+    coordArray = rowCaptureCheck(x, y, 1, 1);
+    if (coordArray) capture(x, y, coordArray[0], coordArray[1]);
     findHighest();
     //slope -1 diagonal check
-    tempPoints = rowCheck(x, y, -1, 1) 
+    tempPoints = rowScoreCheck(x, y, -1, 1);
+    coordArray = rowCaptureCheck(x, y, -1, 1);
+    if (coordArray) capture(x, y, coordArray[0], coordArray[1]);
     findHighest();
-    if (points >= WINNING_LENGTH) {
-      return true;
-    }
+    if (points >= WINNING_LENGTH) return true;
+    if (GameFlow.activePlayer.captureScore >= WINNING_LENGTH) return true;
   }
 
-  //rowCheck
+  //rowScoreCheck
   //it returns the longest possible length,
   //measured from the last-placed piece,
   //where directions are measured by diffX / diffY
-  const rowCheck = function(x, y, diffX, diffY) {
+  const rowScoreCheck = function(x, y, diffX, diffY) {
     let points = 0;
     let tempX = x;
     let tempY = y;
     for (let d = 0; d < WINNING_LENGTH; d++) {
-      if (!findOccupant(tempX, tempY)) {
+      if (!ownsOccupant(tempX, tempY)) {
         break;
       }
       tempX = tempX + diffX;
@@ -139,7 +159,7 @@ const GameBoard = ( () => {
     diffY = diffY * -1;
     points--;
     for (let d = 0; d < WINNING_LENGTH; d++) {
-      if (!findOccupant(tempX, tempY)) {
+      if (!ownsOccupant(tempX, tempY)) {
         break;
       }
       tempX = tempX + diffX;
@@ -148,6 +168,71 @@ const GameBoard = ( () => {
     }
     return points;
   }
+
+  //rowCaptureCheck
+  //returns an array of [diffX, diffY] 
+  //if a capture can be made
+  const rowCaptureCheck = function (x, y, diffX, diffY) {
+    let opponentPieces = 0;
+    let tempX = x + diffX;
+    let tempY = y + diffY;
+    while(true) {
+      if (!isOccupied(tempX, tempY) || ownsOccupant(tempX, tempY)) break;
+      tempX = tempX + diffX;
+      tempY = tempY + diffY;
+      opponentPieces++;
+      if (opponentPieces == 2) {
+        if (ownsOccupant(tempX, tempY)) return [diffX, diffY];
+      }
+    }
+    diffX = diffX * -1;
+    diffY = diffY * -1;
+    tempX = x + diffX;
+    tempY = y + diffY;
+    opponentPieces = 0;
+    while(true) {
+      if (!isOccupied(tempX, tempY) || ownsOccupant(tempX, tempY)) break;
+      tempX = tempX + diffX;
+      tempY = tempY + diffY;
+      opponentPieces++;
+      if (opponentPieces == 2) {
+        if (ownsOccupant(tempX, tempY)) return [diffX, diffY];
+      }
+    }
+    return false;
+  }
+  //capture: removes the pieces from the board
+  //prompts countCapture for scoring & UI
+  const capture = function(x, y, diffX, diffY) {
+    let tempX = x + diffX;
+    let tempY = y + diffY;
+    let gridSquare = document.getElementById(`${tempX} ${tempY}`);
+    gridSquare.removeChild(gridSquare.firstChild);
+    tempX = tempX + diffX;
+    tempY = tempY + diffY;
+    gridSquare = document.getElementById(`${tempX} ${tempY}`);
+    gridSquare.removeChild(gridSquare.firstChild);
+    countCapture();
+  }
+
+  //adds to the player's captureScore, and adds captured pieces to UI
+  const countCapture = function() {
+    GameFlow.activePlayer.captureScore++;
+    let captureDiv = document.getElementById(GameFlow.activePlayer.name);
+    for (let i = 0; i < 2; i++) {
+      let newPiece = document.createElement('div');
+      if (GameFlow.activePlayer.name == playerOne.name) {
+        newPiece.setAttribute('id', playerTwo.color);
+        newPiece.setAttribute('class', 'occupied');
+      } else {
+        newPiece.setAttribute('id', playerOne.color);
+        newPiece.setAttribute('class', 'occupied');
+      }
+      captureDiv.appendChild(newPiece);
+    }
+  }
+
+  
   //sayVictory: displays victory screen, asks GameFlow to end
   const sayVictory = function(winner) {
     let winningText = `${GameFlow.activePlayer.name} has won!`;
@@ -176,6 +261,12 @@ const GameBoard = ( () => {
     while (board.firstChild) {
       board.removeChild(board.firstChild)
     }
+    let captureDivs = document.querySelectorAll('.captures');
+    captureDivs.forEach(function(div) {
+      while(div.firstChild) {
+        div.removeChild(div.firstChild);
+      }
+    });
   }
   //user interaction listener!:
     //click will change css to either:
@@ -185,7 +276,7 @@ const GameBoard = ( () => {
     let squares = [...document.querySelectorAll('div#gameBoard > div')];
     squares.forEach((square) => {
       square.addEventListener('click', function() {
-        if (square.textContent.length) {
+        if (square.firstChild) {
           failMove(square);
         } else {
           GameFlow.activePlayer.makeMove(square);
@@ -195,9 +286,9 @@ const GameBoard = ( () => {
   }
 
   return {displayBoard,
-          resetBoard,
+          resetGame,
           makeMove, 
-          findOccupant,
+          ownsOccupant,
           addListeners,
           clearBoard
           };
@@ -226,6 +317,8 @@ const GameFlow = ( () => {
   const startGame = function() {
     GameBoard.displayBoard();
     GameBoard.addListeners();
+    playerOne.captureScore = 0;
+    playerTwo.captureScore = 0;
     GameFlow.changeTurn();
   }
   //endGame: asks GameBoard to clear the board,
@@ -243,13 +336,14 @@ const GameFlow = ( () => {
 
 //Create a Player factory
 //These:
-//speak to the GameBoard units
+//speak to the GameBoard module
 //hear from the GameFlow module
-const PlayerFactory = (name, symbol, idString) => {
-  let score = 0;
+const PlayerFactory = (name, color, idString) => {
+  let gameScore = 0;
+  let captureScore = 0;
   const scoreboard = document.getElementById(idString);
-  const makeMove = GameBoard.makeMove.bind(symbol);
-  return {name, symbol, score, scoreboard, makeMove};
+  const makeMove = GameBoard.makeMove.bind(color);
+  return {name, color, gameScore, captureScore, scoreboard, makeMove};
 }
 
 
@@ -262,14 +356,18 @@ document.addEventListener("DOMContentLoaded", function() {
   modal.style.display='block';
   let submitButton = document.getElementById('submit');
   submitButton.onclick = function() {
-    pOneName = document.getElementById('playerOne').value;
-    pTwoName = document.getElementById('playerTwo').value;
+    let pOneName = document.getElementById('pOneName').value;
+    let pTwoName = document.getElementById('pTwoName').value;
     pOneName = !pOneName ? 'Player One' : pOneName;
     pTwoName = !pTwoName ? 'Player Two' : pTwoName;
+    let pOneColor = document.getElementById('pOneColor').value;
+    let pTwoColor = document.getElementById('pTwoColor').value;
     modal.style.display = 'none';
+    console.log(pOneColor);
+    console.log(pTwoColor);
     //cast these as global variables to use in modules
-    playerOne = PlayerFactory(pOneName, 'X', 'pOneScore')
-    playerTwo = PlayerFactory(pTwoName, 'O', 'pTwoScore');
+    playerOne = PlayerFactory(pOneName, pOneColor, 'pOneScore')
+    playerTwo = PlayerFactory(pTwoName, pTwoColor, 'pTwoScore');
     GameFlow.startGame();
   }
 });
